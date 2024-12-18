afff18947620057a7e90a296d212c624a0f5cff0
Yernur <yernurzhetkerbayev@gmail.com>
2024-12-18 22:54:32 +0500

    head

--- src/__test__/salescout.test.js
+++ src/__test__/salescout.test.js
@@ -10,12 +10,6 @@ test('filterAndSortProducts should return unique products sorted by price', () =
 
     const result = filterAndSortProducts(products);
 
-    const res = [
-        { name: 'C', price: 10 },
-        { name: 'B', price: 20 },
-        { name: 'A', price: 30 }
-    ]
-
     const expected = [
         { name: 'C', price: 10 },
         { name: 'B', price: 20 },
