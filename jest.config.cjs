2ef8c4867ced0a03a96a199e1802bf8745272efc
Yernur <yernurzhetkerbayev@gmail.com>
2024-12-18 21:47:12 +0500

    head

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,6 +2,8 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  collectCoverage: true,
+  coverageDirectory: "coverage",
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
