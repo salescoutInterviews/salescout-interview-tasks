1bda610146093784dd503f66d90312e2299d207b
Yernur <yernurzhetkerbayev@gmail.com>
2024-12-18 21:57:39 +0500

    head

--- jest.config.cjs
+++ jest.config.cjs
@@ -7,10 +7,9 @@ module.exports = {
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest', // Для TypeScript файлов
-    '^.+\\.js$': 'babel-jest', // Для JavaScript файлов (если используются ES-модули)
+    '^.+\\.js$': 'babel-jest', // Для JavaScript файлов
   },
   roots: ['<rootDir>/src'],
-  extensionsToTreatAsEsm: ['.ts', '.tsx', '.js'], // Укажите расширения, которые должны обрабатываться как ESM
   globals: {
     'ts-jest': {
       useESM: true, // Включаем поддержку ES-модулей в ts-jest
