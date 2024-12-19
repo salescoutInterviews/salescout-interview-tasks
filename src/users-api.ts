46096e795d0f15156624935fac06cc194f2d7e99
Yernur <yernurzhetkerbayev@gmail.com>
2024-12-19 12:57:13 +0500

    head

--- src/users-api.ts
+++ src/users-api.ts
@@ -4,24 +4,24 @@
 
 // Use Express library
 
-import express, { Request, Response } from 'express'; // Импортируем нужные типы
+import express, { Request, Response } from 'express';
 const app = express();
 
 app.use(express.json());
 
-const users: any[] = [];
+const users: { name: string }[] = [];
 
 app.post('/user', (req: Request, res: Response) => {
-    // Your code goes here
     res.status(200).send();
 });
 
 app.get('/users', (req: Request, res: Response) => {
-    // Your code goes here
     res.status(200).json(users);
 });
 
-const PORT = 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+if (process.env.NODE_ENV !== 'test') {
+    const PORT = 3000;
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
 
-module.exports = app;
+export default app;
